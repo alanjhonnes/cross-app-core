e0adfa860b76bc0f91fe9eeb7c3f12edfdda47d4
alan.silva <alan.silva@viverebrasil.com.br>
2016-10-11 10:33:51 -0300

    basic lib

---
+++ auth/index.ts
@@ -0,0 +1,6 @@
+export * from "./auth.actions";
+export * from "./auth.effects";
+export * from "./auth.module";
+export * from "./auth.reducer";
+export * from "./components/login.component";
+export * from "./services/auth.service";
--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
-export * from "./src/core.module";
+export * from "./auth";
+export * from "./core";
+export * from "./utils/type";
